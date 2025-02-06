316d3c2a726d94b6a31a964d871a19911be6cff7
Hoon Oh <2078254+hoonoh@users.noreply.github.com>
2025-02-06 09:18:51 +0900

    fix: update constants (#993)

--- src/constants/ec2-info.ts
+++ src/constants/ec2-info.ts
@@ -223,6 +223,7 @@ export const ec2Info: Record<_InstanceType | string, Ec2InstanceInfo> = {
   'f1.4xlarge': { vCpu: 16, memoryGiB: 244, architectures: ['x86_64'] },
   'f2.12xlarge': { vCpu: 48, memoryGiB: 512, architectures: ['x86_64'] },
   'f2.48xlarge': { vCpu: 192, memoryGiB: 2048, architectures: ['x86_64'] },
+  'f2.6xlarge': { vCpu: 24, memoryGiB: 256, architectures: ['x86_64'] },
   'g4ad.16xlarge': { vCpu: 64, memoryGiB: 256, architectures: ['x86_64'] },
   'g4ad.2xlarge': { vCpu: 8, memoryGiB: 32, architectures: ['x86_64'] },
   'g4ad.4xlarge': { vCpu: 16, memoryGiB: 64, architectures: ['x86_64'] },
